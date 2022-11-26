a3853010f88b32140764f9caad3d74b674b7116a
Your Name <you@example.com>
2022-11-26 21:24:53 +0700

    new file in develop new new

--- main.c
+++ main.c
@@ -4,12 +4,8 @@ int main(){
     setlocale(LC_ALL, "Russian");
     const double pi = 3.14;
     double V, r, h;
-
-
-    
     printf("Введите значения r и h через пробел: ");
     scanf("%lg %lg", &r, &h);
-
     V = pi * r * r * h;
     printf("Объем бассейна равен %lg\n", V);
     return 0;
